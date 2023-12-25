286ca820600da1d06663e4f39a78111567b6cfbc
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-25 18:59:16 +0530

    chore: configure prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  endOfLine: "auto",
+  printWidth: 110,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    /**
+     * **NOTE** tailwind plugin must come last!
+     * @see https://github.com/tailwindlabs/prettier-plugin-tailwindcss#compatibility-with-other-prettier-plugins
+     */
+    "prettier-plugin-tailwindcss",
+  ],
+};
