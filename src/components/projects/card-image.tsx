0b48f358146186faa54f86a0fca8fe5c9eacbadb
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-29 21:45:54 +0530

    fix: project card image skeleton

--- src/components/projects/card-image.tsx
+++ src/components/projects/card-image.tsx
@@ -13,10 +13,9 @@ const CardImage = ({ src, alt, className, ...props }: ComponentProps<typeof Imag
       alt={alt}
       sizes="(max-width: 767px) 75vw, (max-width: 991px) 73vw, (max-width: 1439px) 727.78125px, (max-width: 1919px) 866.375px, 1193.9896240234375px"
       className={cn(
-        "transition-all duration-700",
         isLoading
-          ? "animate-skeleton rounded-lg border bg-gradient-to-r from-neutral-950 via-neutral-700 to-neutral-950 bg-[400%,100%]"
-          : "z-10 object-contain group-hover:-translate-y-2",
+          ? "-mb-4 animate-skeleton rounded-lg bg-gradient-to-r from-neutral-950 via-neutral-700 to-neutral-950 bg-[400%,100%]"
+          : "z-10 object-contain transition-all duration-700 group-hover:-translate-y-2",
         className
       )}
       onLoad={handleSetIsLoaded}
