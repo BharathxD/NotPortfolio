f9fc45a1078143f9c423196e406c755046fff0bb
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2023-12-31 18:45:31 +0530

    chore: remove image remote patterns config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "i.ibb.co",
-        pathname: "/**",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
