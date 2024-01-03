3a2bd7ffdae75c959e6d66affb811a3e4723d9aa
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2024-01-03 18:24:10 +0530

    fix: removed `getIsSSRMobile` func as its stopping the next build system to generate static pages

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,19 +1,14 @@
-import getIsSSRMobile from "~/actions/getIsSSRMobile";
-import dynamic from "next/dynamic";
 import Logo from "../ui/logo";
 import DesktopNav from "./desktop-nav";
 import MobileNav from "./mobile-nav";
 
-const MobileNavSSR = dynamic(() => import("./mobile-nav"), { ssr: false });
-
 const Navbar = () => {
-  const isMobile = getIsSSRMobile();
   return (
     <nav
       className="flex w-full flex-row items-center justify-between p-5 md:flex md:p-10"
       aria-label="Main navigation">
       <Logo />
-      {isMobile ? <MobileNav /> : <MobileNavSSR />}
+      <MobileNav />
       <DesktopNav />
     </nav>
   );
