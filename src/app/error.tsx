01915c6f6685ac2456a57b130684ea3ca5941c73
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2024-01-08 20:37:10 +0530

    chore: improve error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,15 +2,28 @@
 
 import EmptyPage from "~/components/ui/empty-page";
 import { Shell } from "~/components/ui/shell";
+import { useEffect } from "react";
 
-const ErrorPage = () => (
-  <Shell variant="landing">
-    <EmptyPage
-      emoticon="disappointed"
-      message="Whoops! Something went wrong, maybe refresh?"
-      actionName="Refresh"
-    />
-  </Shell>
-);
+interface Props {
+  error: Error & { digest?: string };
+  reset: () => void;
+}
+
+const ErrorPage = ({ error, reset }: Props) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <Shell variant="landing">
+      <EmptyPage
+        emoticon="disappointed"
+        message="Whoops! Something went wrong, maybe refresh?"
+        actionName="Refresh"
+        action={reset}
+      />
+    </Shell>
+  );
+};
 
 export default ErrorPage;
