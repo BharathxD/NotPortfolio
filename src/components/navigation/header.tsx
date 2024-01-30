261acfc39aca586c7e574b85b11d0dd6783f50f3
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2024-01-30 22:40:54 +0530

    chore(deps): update dependencies

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -3,9 +3,7 @@ import DesktopNav from "./desktop-nav";
 import MobileNav from "./mobile-nav";
 
 const Navbar = () => (
-  <nav
-    className="flex w-full flex-row items-center justify-between p-5 md:flex md:p-10"
-    aria-label="Main navigation">
+  <nav className="flex w-full flex-row items-center justify-between p-5 md:p-10" aria-label="Main navigation">
     <Logo />
     <MobileNav />
     <DesktopNav />
