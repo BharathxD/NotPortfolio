cdfe7272d84d28de2a5b0b8317828ad2508d1238
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2024-01-31 20:10:26 +0530

    chore: minor changes and improvements

--- src/components/ui/highlighted-span.tsx
+++ src/components/ui/highlighted-span.tsx
@@ -1,9 +1,7 @@
 interface Props extends React.HTMLAttributes<HTMLSpanElement> {}
 
 const HighlightedSpan = ({ children, ...rest }: Props) => (
-  <span
-    className="bg-highlight bg-clip-text text-2xl font-semibold italic text-neutral-200 text-shadow-lg md:text-4xl"
-    {...rest}>
+  <span className="bg-highlight bg-clip-text font-semibold italic text-neutral-200 text-shadow-lg" {...rest}>
     {" "}
     {children}
     {"  "}
--- src/features/hero/personal-introduction.tsx
+++ src/features/hero/personal-introduction.tsx
@@ -12,10 +12,10 @@ const PersonalIntroduction = () => (
       initial="hidden"
       whileInView="show"
       variants={FADE_DOWN_ANIMATION_VARIANTS}>
-      Hey, I&apos;m <HighlightedSpan>Bharath Lakshman Kumar</HighlightedSpan> (you can call me Bharath,
-      though). I&apos;m a <HighlightedSpan>UI Engineer</HighlightedSpan> and a student based in Hyderabad,
-      India. I&apos;m working as a software engineering <HighlightedSpan>intern</HighlightedSpan> at
-      Vertocity, where I contribute to building a variety of AI/Ed <HighlightedSpan>SaaS</HighlightedSpan>
+      Hey, {`I'm`} <HighlightedSpan>Bharath Lakshman Kumar</HighlightedSpan> (you can call me Bharath,
+      though). {`I'm`} a <HighlightedSpan>UI Engineer</HighlightedSpan> and a student based in Hyderabad,
+      India. {`I'm`} working as a software engineering <HighlightedSpan>intern</HighlightedSpan> at Vertocity,
+      where I contribute to building a variety of AI/Ed <HighlightedSpan>SaaS</HighlightedSpan>
       products. My background is in UI development, but I love everything related to
       <HighlightedSpan>Server-Side Engineering</HighlightedSpan>
     </m.h1>
