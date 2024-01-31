7f52df5aca6f086dbc845511d733db028eb76319
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2024-01-31 19:55:21 +0530

    chore: remove lazy-import for the intro component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import { Shell } from "~/components/ui/shell";
-import dynamic from "next/dynamic";
-
-const PersonalIntroduction = dynamic(() => import("~/features/hero/personal-introduction"), { ssr: false });
+import PersonalIntroduction from "~/features/hero/personal-introduction";
 
 const HeroPage = () => (
   <Shell>
