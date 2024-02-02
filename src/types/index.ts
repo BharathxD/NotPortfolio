f24927b55a8f1ea1a35624df2f93d450a820a6c4
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2024-02-02 18:36:09 +0530

    chore(d0cs): remove unnecessary documentation

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,13 +9,9 @@ type UniqueIdentifier = string | number;
  * Represents social profiles of a user.
  */
 interface SocialProfiles {
-  /** The platform of the social profile, can be either LinkedIn or Github. */
   platform: "LinkedIn" | "Github";
-  /** The URL of the social profile. */
   url: string;
-  /** The URL of the image of the social profile. */
   imageUrl: string;
-  /** The URL of the blurred data of the image of the social profile. Can be null. */
   blurDataUrl: string | null;
 }
 
@@ -23,21 +19,13 @@ interface SocialProfiles {
  * Represents a project.
  */
 interface Project {
-  /** The unique identifier of the project. */
   id: UniqueIdentifier;
-  /** The name of the project. */
   name: string;
-  /** The tagline of the project. */
   tagline: string;
-  /** The URL of the project. */
   projectUrl: string;
-  /** The Github URL of the project. */
   github: string;
-  /** The number of pictures in the project. */
   noOfPictures: number;
-  /** The points of the project. */
   points: string[];
-  /** The technology stack of the project. */
   stack: string[];
 }
 
@@ -45,17 +33,11 @@ interface Project {
  * Represents the academic background of a user.
  */
 interface AcademicBackground {
-  /** The institution of the academic background. */
   institution: string;
-  /** The degree obtained in the academic background. */
   degreeObtained: string;
-  /** The location of the academic background. */
   location: string;
-  /** The grade point average of the academic background. */
   gradePointAverage: string;
-  /** The major subject of the academic background. */
   majorSubject: string;
-  /** The study period of the academic background. */
   studyPeriod: string;
 }
 
@@ -63,15 +45,10 @@ interface AcademicBackground {
  * Represents a professional certification of a user.
  */
 interface ProfessionalCertifications {
-  /** The name of the certification. */
   name: string;
-  /** The organization that issued the certification. */
   issuingOrganization: string;
-  /** The date the certification was issued. */
   issueDate: string;
-  /** The ID of the certification. */
   credentialId: string;
-  /** The URL of the certification. */
   credentialUrl: string;
 }
 
@@ -79,25 +56,15 @@ interface ProfessionalCertifications {
  * Represents the biography of a user.
  */
 interface Biography {
-  /** The full name of the user. */
   fullName: string;
-  /** The profession of the user. */
   profession: string;
-  /** The contact email of the user. */
   contactEmail: string;
-  /** The contact number of the user. */
   contactNumber: string;
-  /** The social profiles of the user. */
   socialProfiles: SocialProfiles[];
-  /** The link to the resume of the user. */
   resumeLink: string;
-  /** The personal description of the user. */
   personalDescription: string;
-  /** The academic background of the user. */
   academicBackground: AcademicBackground;
-  /** The portfolio projects of the user. */
   portfolioProjects: Project[];
-  /** The professional certifications of the user. */
   professionalCertifications: ProfessionalCertifications[];
 }
 
