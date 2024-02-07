4dc3eea8030196a3e192483d92beaa5967bcb90f
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2024-02-07 20:27:09 +0530

    chore(refactor): `PostCard` component to use destructuring

--- src/features/blog/post-card.tsx
+++ src/features/blog/post-card.tsx
@@ -9,26 +9,29 @@ interface Props {
   post: Post;
 }
 
-const PostCard = ({ post }: Props) => (
-  <li className="list-none" role="listitem" aria-label={`Post card for ${post.title}`}>
-    <Link key={post.slug} href={post.slug} className="w-full" aria-label={post.title}>
-      <article className="relative flex w-full flex-col gap-4 overflow-hidden rounded-xl border bg-neutral-900/80 p-4">
-        <Suspense fallback={<Skeleton className="relative z-10 h-5 w-16" />}>
-          <Views slug={post.slug} />
-        </Suspense>
-        <div className="absolute inset-0 z-0 bg-card-even" />
-        <div className="absolute inset-x-0 top-0 z-20 h-px bg-glare opacity-20" />
-        <div className="absolute inset-x-0 bottom-0 z-20 h-px bg-glare opacity-20" />
-        <h2 className="relative z-10 bg-gradient-to-tr from-neutral-50 via-neutral-200 to-neutral-500 bg-clip-text text-xl font-medium text-transparent duration-1000 group-hover:text-white lg:text-3xl">
-          {post.title}
-        </h2>
-        <p className="relative z-10 line-clamp-2 text-sm duration-1000 group-hover:text-zinc-200">
-          {post.description}
-        </p>
-        <p className="relative z-10 text-sm text-muted-foreground">{formatDate(post.date)}</p>
-      </article>
-    </Link>
-  </li>
-);
+const PostCard = ({ post }: Props) => {
+  const { slug, title } = post;
+  return (
+    <li className="list-none" role="listitem" aria-label={`Post card for ${title}`}>
+      <Link key={slug} href={slug} className="w-full" aria-label={title}>
+        <article className="relative flex w-full flex-col gap-4 overflow-hidden rounded-xl border bg-neutral-900/80 p-4">
+          <Suspense fallback={<Skeleton className="relative z-10 h-5 w-16" />}>
+            <Views slug={slug} />
+          </Suspense>
+          <div className="absolute inset-0 z-0 bg-card-even" />
+          <div className="absolute inset-x-0 top-0 z-20 h-px bg-glare opacity-20" />
+          <div className="absolute inset-x-0 bottom-0 z-20 h-px bg-glare opacity-20" />
+          <h2 className="relative z-10 bg-gradient-to-tr from-neutral-50 via-neutral-200 to-neutral-500 bg-clip-text text-xl font-medium text-transparent duration-1000 group-hover:text-white lg:text-3xl">
+            {title}
+          </h2>
+          <p className="relative z-10 line-clamp-2 text-sm duration-1000 group-hover:text-zinc-200">
+            {post.description}
+          </p>
+          <p className="relative z-10 text-sm text-muted-foreground">{formatDate(post.date)}</p>
+        </article>
+      </Link>
+    </li>
+  );
+};
 
 export default PostCard;
