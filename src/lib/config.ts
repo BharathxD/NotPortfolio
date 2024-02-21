f2f587e1f94198e73c1122c430a978c1813bba1d
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2024-02-21 16:00:36 +0530

    chore: Update favicon and apple-touch-icon URLs

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -46,17 +46,17 @@ const siteConfig = {
   keywords,
   favicons: [
     {
-      url: "/public/favicon-16x16.png",
+      url: "/favicon-16x16.png",
       sizes: "16x16",
       type: "image/png",
     },
     {
-      url: "/public/favicon-32x32.png",
+      url: "/favicon-32x32.png",
       sizes: "32x32",
       type: "image/png",
     },
     {
-      url: "/public/apple-touch-icon.png",
+      url: "/apple-touch-icon.png",
       sizes: "180x180",
       type: "image/png",
     },
