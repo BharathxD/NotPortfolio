d16fb38728b9b86572c8f79b9bedb5f667bb3f10
Bharath Lakshman Kumar <108531789+BharathxD@users.noreply.github.com>
2024-02-21 16:54:22 +0530

    fix: use project name as the identifier

--- src/features/projects/project-carousel.tsx
+++ src/features/projects/project-carousel.tsx
@@ -9,7 +9,11 @@ import ImageWithLoader from "~/components/ui/image-with-loader";
 import type { Project } from "~/types";
 import { memo } from "react";
 
-const ProjectCarousel = ({ id, noOfPictures, tagline }: Pick<Project, "id" | "noOfPictures" | "tagline">) => (
+const ProjectCarousel = ({
+  name,
+  noOfPictures,
+  tagline,
+}: Pick<Project, "name" | "noOfPictures" | "tagline">) => (
   <Carousel className="relative rounded-3xl border border-neutral-700 bg-neutral-950 p-2 shadow-lg">
     <div className="absolute inset-x-0 top-0 z-20 h-px bg-glare opacity-80" />
     <div className="absolute inset-x-0 bottom-0 z-20 h-px bg-glare opacity-80" />
@@ -18,7 +22,7 @@ const ProjectCarousel = ({ id, noOfPictures, tagline }: Pick<Project, "id" | "no
         <CarouselItem key={i} className="relative w-full">
           <ImageWithLoader
             key={`${i}_${i + 1}`}
-            src={`/_work/${id}/${i + 1}.webp`}
+            src={`/_work/${name.toLocaleLowerCase()}/${i + 1}.webp`}
             alt={tagline}
             className="z-10 w-full rounded-2xl border"
             height={1878}
