c864de3a902f7c93552b049ccd263ed9bb50fd52
Bharath Lakshman Kumar <bharathxxd@gmail.com>
2025-04-15 20:42:58 +0530

    refactor: streamline GET handler by consolidating waitUntil logic and enhancing error handling

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { waitUntil } from "@vercel/functions/wait-until";
+import { detectBot } from "~/lib/detect-bot";
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import { NextResponse } from "next/server";
+
+const getRedirectUrl = async (req: Request) => {
+  await new Promise((resolve) => {
+    const isBot = detectBot(req);
+    console.info("isBot:inner", { isBot });
+    resolve(true);
+  });
+};
+
+const middleware = (req: Request) => {
+  const isBot = detectBot(req);
+
+  if (isBot) {
+    return NextResponse.json({ error: "Bot detected" }, { status: 403 });
+  }
+
+  console.log("redirecting to /");
+  console.log("req.url", req.url);
+  console.log("isBot", isBot);
+  waitUntil(getRedirectUrl(req));
+
+  return NextResponse.next();
+};
+
+export default middleware;
